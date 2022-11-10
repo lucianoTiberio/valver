ed860d041ca9f104fed815a80f48df585352695c
tiberio <lucianotib2014@gmail.com>
2022-11-10 11:23:26 -0300

    git add

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-import pandas as pd
+from pandas import DataFrame,read_excel
 import psycopg2 as psycopg2
-import os
-import time
+from os import remove,system
 from datetime import datetime
 
 try:
-    conexao = psycopg2.connect(host='', database='',user='',password='')
+    conexao = psycopg2.connect()
     cursor = conexao.cursor()
 
-    df = pd.read_excel("posto_vaver.xlsx", engine='openpyxl')
+    df = read_excel("posto_vaver.xlsx", engine='openpyxl')
     contador = int(len(df['EAN']))
 
     def dados():
         i = 0
         resultado = []
         while i < contador:
-            comando = f"select * from backup.consulta_precos where codigobarras = lpad({df['EAN'][i]},13,0)"
-            cursor.execute(comando)
-            resultado.append(cursor.fetchall()[0])
-            i += 1
+            try:
+                comando = f"select * from backup.consulta_precos where codigobarras = lpad({df['EAN'][i]},13,0)"
+                cursor.execute(comando)
+                resultado.append(cursor.fetchall()[0])
+                i += 1
+            except:
+                print("O produto ", df['EAN'][i]," informado esta errado valide com a loja" )
+                i += 1
         return resultado
 
     def gerador():
 
         dia = datetime.now().strftime('%Y-%m-%d')
-        new_df = pd.DataFrame(data=dados())
+        new_df = DataFrame(data=dados())
         new_df.to_excel(f'Produtos&Updates--{dia}.xlsx',index=False,header=['Valor','EAN','Descrição','Update'])
-        os.remove('posto_VAVER.xlsx')
+        remove('posto_VAVER.xlsx')
         print("Arquivo gerado com sucesso")
 
 
     gerador()
-    time.sleep(3)
+    system('pause')
 except:
     print('O arquivo não se encontra na pasta ou nome do arquivo esta incorreto')
     print('O deve estar no formato xlsx com o nome posto_vaver.xlsx ')
-    time.sleep(3)
+    system('pause')
